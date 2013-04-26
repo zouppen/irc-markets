2faff8246d7dfe1ac0417d6fa2cf20f528d2390d
Joel Lehtonen <joel.lehtonen@iki.fi>
2013-04-26 03:22:56 +0300

    ByteString values in topic storage

--- src/Irc.hs
+++ src/Irc.hs
@@ -1,14 +1,17 @@
-module Main where
+module Irc where
 
 import System.IO
 import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TVar
+import Data.ByteString.Char8 (ByteString,empty,pack,unpack)
 import Network
 import Network.IRC
 import Data.Map (Map)
 import qualified Data.Map as M
+
+type TopicVar = TVar (Map String ByteString)
 
 -- | Connects to Irssi proxy with given hostname, port and password.
 connectIrssiProxy :: HostName -> PortID -> String -> IO Handle
@@ -21,8 +24,8 @@ connectIrssiProxy host port pass = do
   return h
 
 -- | Forks new thread which parses irc topics and keeps them in a map.
-inputChan :: Handle -> IO (TVar (Map String String))
-inputChan h = do
+topicGuard :: Handle -> IO TopicVar
+topicGuard h = do
   var <- newTVarIO M.empty
   forkIO $ forever $ do
     s <- hGetLine h
@@ -32,16 +35,25 @@ inputChan h = do
   return var
 
 -- | Parse if given message has topic change
-topicChange :: Message -> Maybe (String,String)
-topicChange (Message _ "TOPIC" [chan,msg]) = Just (chan,msg)
-topicChange (Message _ "332" [_,chan,msg]) = Just (chan,msg)
+topicChange :: Message -> Maybe (String,ByteString)
+topicChange (Message _ "TOPIC" [chan,msg]) = Just (chan,pack msg)
+topicChange (Message _ "332" [_,chan,msg]) = Just (chan,pack msg)
 topicChange _ = Nothing
 
--- | Debug view of topic table
-showTopics :: TVar (Map String String) -> IO ()
+-- | Debug view of topic table. Doesn't handle character sets.
+showTopics :: TopicVar -> IO ()
 showTopics var = readTVarIO var >>= putStr . unlines . map pair . M.toList
-  where pair (a,b) = a++": "++b
+  where pair (a,b) = a++": "++unpack b
 
 -- | Sets channel topic
-setTopic :: Handle -> String -> String -> IO ()
-setTopic h chan s = hPutStrLn h $ encode $ Message Nothing "TOPIC" [chan,s]
+setTopic :: Handle -> String -> ByteString -> IO ()
+setTopic h chan s = hPutStrLn h $ encode $ Message Nothing "TOPIC"
+                    [chan,unpack s]
+
+-- | Gets topic contents of given channel. Returns empty string if
+-- channel is not known (yet).
+getTopic :: TopicVar -> String -> IO ByteString
+getTopic var chan = do
+  m <- atomically $ readTVar var
+  return $ M.findWithDefault empty chan m
+    
